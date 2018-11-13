a86cef8787b7b6a02ddc206e79616186660564a3
Sebastian Porto <s@porto5.com>
2018-11-13 21:33:01 +1100

    Improve tests

--- tests.ml
+++ tests.ml
@@ -1,8 +1,5 @@
 (* Fixtures *)
 
-let (header_fixture:Jwt.header) =
-	{alg = Jwt.HS256; typ = None}
-
 let header_json =
 	"{\"alg\":\"HS256\"}"
 
@@ -14,23 +11,26 @@ let payload_fixture =
 let secret =
 	"My$ecretK3y"
 
-let (unsigned_token_fixture:Jwt.unsigned_token) =
+let header_fixture (alg:Jwt.algorithm) : Jwt.header =
+	{alg = alg; typ = None}
+
+let unsigned_token_fixture (alg:Jwt.algorithm) : Jwt.unsigned_token =
 	{
-		header = header_fixture;
+		header = header_fixture alg;
 		payload = payload_fixture;
 	}
 
-let (signed_token_fixture:Jwt.t) =
+let signed_token_fixture (alg:Jwt.algorithm) : Jwt.t =
 	{
-		header = unsigned_token_fixture.header;
-		payload = unsigned_token_fixture.payload;
-		signature = Jwt.sign secret unsigned_token_fixture;
+		header = (unsigned_token_fixture alg).header;
+		payload = (unsigned_token_fixture alg).payload;
+		signature = Jwt.sign secret (unsigned_token_fixture alg);
 	}
 
-let token =
-	"eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoic29tZUB1c2VyLnRsZCJ9.kWOVtIOpWcG7JnyJG0qOkTDbOy636XrrQhMm_8JrRQ8"
-
 (* Test helpes *)
+
+let alg_to_str =
+	Jwt.algorithm_to_string
 
 let compareT =
 	Alcotest.testable
@@ -67,7 +67,7 @@ let encode_header () =
 	Alcotest.(check string)
 		"It encodes"
 		header_json
-		(Jwt.header_to_string header_fixture)
+		(Jwt.header_to_string (header_fixture Jwt.HS256))
 
 
 let header_to_string = [
@@ -106,17 +106,17 @@ let encode =
 	data 
 		|> List.map 
 		(fun d ->
-			("It encodes", `Quick, encode_test d)
+			("It encodes " ^ alg_to_str d.alg, `Quick, encode_test d)
 		)
 
 
-(* Test Decoding *)
+(* Decoding *)
 
-let decode_test () =
+let decode_test (data:data) () =
 	Alcotest.(check resultT)
 		"decodes"
-		(Ok signed_token_fixture)
-		(Jwt.decode token)
+		(Ok (signed_token_fixture data.alg))
+		(Jwt.decode data.token)
 
 let decode_fail_test () =
 	Alcotest.(check resultT)
@@ -124,52 +124,73 @@ let decode_fail_test () =
 		(Error "Bad token")
 		(Jwt.decode "Monkey")
 
-let decode = [
-	"It decodes", `Quick, decode_test;
-	"It can fail to decode", `Quick, decode_fail_test;
-]
+let decode =
+	data
+		|> List.map
+		(fun d ->
+			[
+				"It decodes " ^ alg_to_str d.alg, `Quick, decode_test d;
+				"It can fail to decode", `Quick, decode_fail_test;
+			]
+		)
+		|> List.flatten
 
-let is_valid_true () =
+(* Validate *)
+
+let is_valid_true (data:data) () =
 	Alcotest.(check bool)
 		"true"
 		true
-		(Jwt.is_valid secret signed_token_fixture)
+		(Jwt.is_valid secret (signed_token_fixture data.alg))
 
-let is_valid_false () =
+let is_valid_false (data:data) () =
 	Alcotest.(check bool)
 		"false"
 		false
-		(Jwt.is_valid "xyz" signed_token_fixture)
+		(Jwt.is_valid "xyz" (signed_token_fixture data.alg))
 
-let is_valid = [
-	"It returns true when valid", `Quick, is_valid_true;
-	"It returns false when invalid", `Quick, is_valid_false;
-]
+let is_valid = 
+	data
+		|> List.map
+		(fun d ->
+			[
+				alg_to_str d.alg ^ " true when valid", `Quick, is_valid_true d;
+				alg_to_str d.alg ^ " false when invalid", `Quick, is_valid_false d;
+			]
+		)
+		|> List.flatten
 
-let decode_and_verify_ok () =
+(* Verify *)
+
+let decode_and_verify_ok (data:data) () =
 	Alcotest.(check resultT)
 		"decodes"
-		(Ok signed_token_fixture)
-		(Jwt.decode_and_verify secret token)
+		(Ok (signed_token_fixture data.alg))
+		(Jwt.decode_and_verify secret data.token)
 
-let decode_and_verify_err () =
+let decode_and_verify_err (data:data) () =
 	Alcotest.(check resultT)
 		"decodes"
 		(Error "Invalid token")
-		(Jwt.decode_and_verify "xyz" token)
+		(Jwt.decode_and_verify "xyz" data.token)
 
-let decode_and_verify = [
-	"It decodes when valid", `Quick, decode_and_verify_ok;
-	"It doesn't decode when invalid", `Quick, decode_and_verify_err;
-]
+let decode_and_verify = 
+	data
+		|> List.map
+		(fun d ->
+			[
+				alg_to_str d.alg ^ " decode when valid", `Quick, decode_and_verify_ok d;
+				alg_to_str d.alg ^ " fails when invalid", `Quick, decode_and_verify_err d;
+			]
+		)
+		|> List.flatten
 
 let () =
 	Alcotest.run "JWT" [
 		"Encode header", header_to_string;
-		(* "Decode header", header_decode_tests; *)
 		"Encode payload", payload_to_string;
 		"Encode JWT", encode;
 		"Decode token", decode;
-		"Verify JWT", is_valid;
+		"Validate JWT", is_valid;
 		"Decode and verify", decode_and_verify;
 	]
