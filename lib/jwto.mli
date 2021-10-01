05cb9cad0a73e50bd57f42cc766a49f3927f59c2
Sebastian Porto <s@porto5.com>
2021-10-01 16:40:44 +1000

    Update docs

--- lib/jwto.mli
+++ lib/jwto.mli
@@ -3,33 +3,44 @@ type algorithm =
   | HS512
   | Unknown
 
+(** The JWT type.
+
+Which has a
+- header
+- payload
+- and signature
+*)
 type t
 
+(** A header has the algorithm used and the type of the token which is JWT  *)
 type header
 
+(** The payload of the token is a list of tuples, where each tuple is a "claim"  *)
 type payload = (string * string) list
 
+(** A JWT that hasn't been signed yet *)
 type unsigned_token
 
 (* Encode, decode, verify *)
 
-(*
-   Encode a token
-
-   	Jwt.encode
+val encode :
+  algorithm -> string -> payload -> (string, string) result
+(** Encode a token
+{[
+   	Jwto.encode
    		Jwt.HS256
    		"secret"
    		payload
+]}
 *)
-val encode :
-  algorithm -> string -> payload -> (string, string) result
 
-(*
-   Decode a token, this doesn't verify
-
-   	Jwt.decode "eyJhbGciOiJIUzI1NiJ9...."
-*)
 val decode : string -> (t, string) result
+(** Decode a token, this doesn't verify.
+Use decode_and_verify to do both
+{[
+   	Jwto.decode "eyJhbGciOiJIUzI1NiJ9...."
+]}
+*)
 
 (* Getters *)
 
@@ -39,45 +50,66 @@ val get_payload : t -> payload
 
 val get_signature : t -> string
 
-(*
-Decode and verify
-
-	Jwt.decode_and_verify
-		"secret" "eyJhbGciOiJIUzI1NiJ9...."
-*)
 val decode_and_verify : string -> string -> (t, string) result
+(**
+Decode and verify
+{[
+    Jwto.decode_and_verify
+        "secret" "eyJhbGciOiJIUzI1NiJ9...."
+]}
+*)
 
 val is_valid : string -> t -> bool
+(** Check if a token is valid. The first argument is the secret.
+{[
+	Jwto.is_valid "secret" "eyJhbGciOiJIUzI1NiJ9...."
+]}
+*)
 
 (* Build tokens manually *)
 
 val make_header : algorithm -> header
+(** Make a header given the algorithm  *)
 
 val make_unsigned_token : header -> payload -> unsigned_token
+(** Make an unsigned token  *)
 
 val make_signed_token :
   string -> unsigned_token -> (t, string) result
+(** Make a signed token. This can fail if Base64 fails to encode the header or payload.
+{[
+	unsigned_token
+	|> Jwto.make_signed_token "secret"
+]}
+*)
 
 (* Utility functions *)
 
 val pp : Format.formatter -> t -> unit
+(** Pretty print a token  *)
 
-(* Compare two tokens *)
 val eq : t -> t -> bool
+(** Compare two tokens *)
 
 val algorithm_to_string : algorithm -> string
+(** Converts the algorithm type to a string *)
 
 val show_algorithm : algorithm -> string
 
-(* Print header as JSON string *)
 val header_to_string : header -> string
+(** Encode the header as a JSON string *)
 
 val show_header : header -> string
 
-(* Print payload as JSON string *)
 val payload_to_string : payload -> string
+(** Encode the payload as a JSON string *)
 
 val show_payload : payload -> string
 
-(* Get a claim from the payload *)
 val get_claim : string -> payload -> string option
+(** Get a claim from the payload
+{[
+	payload
+	|> Jwto.get_claim "email"
+]}
+*)
